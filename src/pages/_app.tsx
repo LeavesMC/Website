156f8545ecf7c5a3c44f9fe6d67b992d3b210a5f
LittleChest <81231195+LittleChest@users.noreply.github.com>
2023-08-15 01:59:46 +0800

    feat: add Umami

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,16 @@ import NavBar from "@/components/layout/NavBar";
 
 const MyApp = ({ Component, pageProps, router }: AppProps) => (
   <>
+    <script async src="https://umami.skinow.skin/script.js"
+      data-website-id="2b1cd1c4-1779-4bfa-841c-7106cb7733f8"
+      data-do-not-track="true"
+      data-domains="leavesmc.top"
+    ></script>
+    <script async src="https://umami.skinow.skin/script.js"
+      data-website-id="09b01113-976e-444b-b10e-b3550f5184a9"
+      data-do-not-track="true"
+      data-domains="www.leavesmc.top"
+    ></script>
     <NavBar component={Component} />
     <main className="flex-1">
       <Component {...pageProps} />
