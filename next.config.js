0f9dd91dd97b8a3651eac3bfc5b199cfd9312bd6
LittleChest <81231195+LittleChest@users.noreply.github.com>
2024-07-18 14:31:14 +0000

    chore: Do not use tunnelRoute bump: Update Sentry

--- next.config.js
+++ next.config.js
@@ -41,12 +41,6 @@ module.exports = withSentryConfig(module.exports, {
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/monitoring",
-
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
 
