53935dc835d0f562b638c37708063f576a1c5489
LittleChest <hiya@littlew.top>
2026-03-18 21:50:47 +0800

    chore: i18n: Remove Chinese support fix #34

--- i18n.ts
+++ i18n.ts
@@ -4,14 +4,12 @@ import { initReactI18next } from "react-i18next";
 
 import enUS from "src/i18n/en.json";
 import frFR from "src/i18n/fr-FR.json";
-import zhCN from "src/i18n/zh-CN.json";
-import zhTW from "src/i18n/zh-TW.json";
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: ["en-US", "zh-CN", "zh-TW", "fr-FR"],
+    fallbackLng: ["en-US", "fr-FR"],
     debug: false,
     interpolation: {
       escapeValue: false,
@@ -22,12 +20,6 @@ i18n
       },
       "fr-FR": {
         translation: frFR,
-      },
-      "zh-CN": {
-        translation: zhCN,
-      },
-      "zh-TW": {
-        translation: zhTW,
       },
     },
   });
