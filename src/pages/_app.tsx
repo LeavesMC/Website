01f7e89e57a84a0e19661e24eb986599de278ae4
LittleChest <hiya@littlew.top>
2026-03-31 22:54:25 +0800

    fun: 2026

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,7 @@ i18n;
 
 const MyApp = ({ Component, pageProps, router }: AppProps) => (
   <>
+    <script defer type="text/javascript" src="/2026.js" />
     <script
       async
       type="text/javascript"
